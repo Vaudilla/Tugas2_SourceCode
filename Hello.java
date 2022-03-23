d41b0b08d90f432b8679d5c78eb182cf35b7c5ef
Vaudilla <101856733+Vaudilla@users.noreply.github.com>
2022-03-23 18:57:30 +0700

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,20 @@
+/* 13020200017, vaudilla, Minggu 20 Maret 2022 */
+public class Hello {
+    /**
+     * @param args
+     */
+public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    /* menuliskan hello ke layar */
+    System.out.print("Hello");
+ 
+   /* menuliskan hello dan ganti baris*/
+   System.out.print("\nHello ");
+
+   /* menuliskan hello dan ganti baris*/
+   System.out.println("World");
+
+   System.out.println("Welcome");
+
+     }
+}
